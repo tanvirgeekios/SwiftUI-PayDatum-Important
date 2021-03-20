f17cdd0a4953866302bf3b9dc0e01fc31a5dfd26
tanvirgeekios <tanvir.alam@iosventure.com>
2021-03-20 09:47:51 +0600

    Initial Commit

---
+++ PayDatum-HomeScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PayDatum-HomeScreen
+//
+//  Created by MD Tanvir Alam on 20/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PayDatum-HomeScreen/PayDatum_HomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayDatum_HomeScreenApp.swift
+//  PayDatum-HomeScreen
+//
+//  Created by MD Tanvir Alam on 20/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PayDatum_HomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
